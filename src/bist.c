f2769f99b598cc26d322d7e6311818e243d6d1a0
Angus Logan <awjlogan@gmail.com>
2025-01-02 13:24:00 +0000

    [BIST] Add stub for BIST entry

---
+++ src/bist.c
@@ -0,0 +1,12 @@
+#include "bist.h"
+#include "board_def.h"
+#include "emon32_samd.h"
+
+void bistRun(void) {
+  /* This routine checks:
+   *   - Value of the analog reference
+   *   - Value of the rail split
+   *   - Value of the ADC calibration inputs
+   *   -
+   */
+}
---
+++ src/bist.h
@@ -0,0 +1,4 @@
+#pragma once
+
+/* @brief Runs the built in self test */
+void bistRun(void);
